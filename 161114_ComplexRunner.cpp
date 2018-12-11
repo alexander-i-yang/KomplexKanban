cfc52a24911aa2aca78369b3570d9e51ccb38a10
AlexY <alex@yang2k.com>
2018-12-11 11:41:31 -0600

    Update 161114_ComplexRunner.cpp

--- 161114_ComplexRunner.cpp
+++ 161114_ComplexRunner.cpp
@@ -8,10 +8,10 @@
 int main () {
     std::cout << "Hello World" << std::endl;
 
-    Complex a(5.0,6.0),b(1.0,1.0);
+    Complex a(5.0,6.0),b;
 
-    /*std::cout << "Enter b: ";
-    std::cin >> b;*/
+    std::cout << "Enter b: ";
+    std::cin >> b;
 
     std::cout << "a = " << a << "\n";
     std::cout << "b = " << b << "\n";
@@ -41,13 +41,33 @@ int main () {
     cout << "The sin is: " << sin(z1) << "\n";
 
     /* other function testing */
-    cout << "The acosh is: " << acosh(a) << "\n";
-    cout << "The asec is: " << asec(a) << "\n";
-    cout << "The acot is: " << acot(a) << "\n";
+    cout << "\n------ OTHER FUNCTION TESTING ------\n\n";
+    cout << "The real part is: " << real(a) << "\n";
+    cout << "The imaginary part is: " << imag(a) << "\n";
+    cout << "The log (base 10) is: " << log10(a) << "\n";
+    cout << "a^b is: " << pow(a, b) << "\n";
+    cout << "a^2.7 is: " << pow(a, 2.7) << "\n";
+    cout << "2.7^a is: " << pow(2.7, a) << "\n";
+    cout << "The cube root is: " << root(a, 3) << "\n\n";
+
+    cout << "(a == b) is: " << ((a==b) ? "true" : "false") << "\n";
+    cout << "(a != b) is: " << ((a!=b) ? "true" : "false") << "\n\n";
+
+    cout << "sin(a) = " << sin(a) << "\n";
+    cout << "cos(a) = " << cos(a) << "\n";
+    cout << "tan(a) = " << tan(a) << "\n\n";
+
     cout << "The acsc is: " << acsc(a) << "\n";
-    // cout << "The asech is: " << asech(a) << "\n";
-    cout << "The acoth is: " << acoth(a) << "\n";
+    cout << "The asec is: " << asec(a) << "\n";
+    cout << "The acot is: " << acot(a) << "\n\n";
+
+    cout << "The asinh is:" << asinh(a) << "\n";
+    cout << "The acosh is: " << acosh(a) << "\n";
+    //TODO: implement the rnd function
     // cout << "Rounded to 2 decimal places: " << acosh(z1).rnd(2) << "\n";
     cout << "The atanh is: " << atanh(a) << "\n";
+    cout << "The acsch is: " << acsch(a) << "\n";
+    cout << "The asech is: " << asech(a) << "\n";
+    cout << "The acoth is: " << acoth(a) << "\n";
     cout << "The complex number rotated by pi/6 radians is: " << rotate(a, PI/6) << "\n";
 }
