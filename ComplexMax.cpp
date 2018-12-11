ed13e1b120b9d168e60be08432ee04a69bc3f29e
AlexY <alex@yang2k.com>
2018-12-11 11:42:35 -0600

    Update ComplexMax.cpp

--- ComplexMax.cpp
+++ ComplexMax.cpp
@@ -4,6 +4,16 @@
 #define PI 3.1415926538979323846
 #define EULER 2.71828182845904523536
 
+Complex Complex::operator/(double a) {
+	Complex ret (re/a, im/a);
+	return ret;
+}
+
+Complex operator/(double a, const Complex &com) {
+    Complex ret = a/norm(com)*conj(com);
+    return ret;
+}
+
 Complex asinh(Complex com) {
 	Complex ret = log(sqrt(pow(com, 2)+1)+com);
 	return ret;
